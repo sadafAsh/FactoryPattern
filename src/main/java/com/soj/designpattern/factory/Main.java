f70b52b4aad50e7e8d2bfae5a1a5b448409fd88b
test@gmail.com <test@gmail.com>
2022-02-06 19:56:09 +0000

    init commit

---
+++ src/main/java/com/soj/designpattern/factory/Main.java
@@ -0,0 +1,12 @@
+package com.soj.designpattern.factory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    static final Logger logger= LogManager.getLogger(Main.class.getName());
+    public static void main(String[] args){
+
+
+    }
+}
